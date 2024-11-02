6b79ba2b9455b6a245523d75cc152f7ac20ff092
moliinyk <maks.oliynik228@gmail.com>
2024-11-03 00:53:01 +0200

    Added CORS to app router

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.dependencies import client, database
 from app.utils.constants import Dependencies as DP
 from app.routers.users import user_router
 from app.routers.jenkins_logs import jenkins_log_router
 from app.routers.jenkins_histories import jenkins_history_router
 
+
+load_dotenv()
 
 app = FastAPI(
     title="Jenkins log parser API",
@@ -14,6 +19,14 @@ app = FastAPI(
 app.include_router(user_router)
 app.include_router(jenkins_log_router)
 app.include_router(jenkins_history_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv("API_URI"), os.getenv("APP_URI")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
